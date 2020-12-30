a6e555d0252c5d0d3d88ea2695d1d5839a29ea47
Ashish <ashishramachandran804@gmail.com>
2020-12-30 15:18:31 -0800

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB9IaAqLHRCwBN8fyw12gOtZdtoaeIDvL0",
+    authDomain: "ecomm-clone-8906a.firebaseapp.com",
+    projectId: "ecomm-clone-8906a",
+    storageBucket: "ecomm-clone-8906a.appspot.com",
+    messagingSenderId: "920823129111",
+    appId: "1:920823129111:web:45144aab984232e5c16852",
+    measurementId: "G-PE882PM68D"
+  };
