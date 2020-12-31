86b5c5e39e1fb7100b88061ad7f422fa36485a88
Ashish <ashishramachandran804@gmail.com>
2020-12-31 13:36:33 -0800

    Added firebase auth for login component

--- src/App.js
+++ src/App.js
@@ -3,21 +3,49 @@ import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Checkout from './components/Checkout/Checkout';
+import Login from './components/Login/Login';
+import { useEffect } from 'react';
+import { auth } from './firebase';
+import { useStateValue } from './StateProvider';
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    auth
+      .onAuthStateChanged(authUser => {
+        if(authUser) {
+          dispatch({
+            type: "SET_USER",
+            user: authUser
+          })
+        } else {
+          dispatch({
+            type: "SET_USER",
+            user: null
+          })
+        }
+      })
+  }, [])
+
   return (
     <Router>
       <div className="app">
 
-        <Header />
         <Switch>
 
           <Route path="/home">
+            <Header />
             <Home />
           </Route>
 
           <Route path="/checkout">
+            <Header />
             <Checkout />
+          </Route>
+
+          <Route path="/login">
+            <Login />
           </Route>
 
           <Redirect to="/home" />
--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,18 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyB9IaAqLHRCwBN8fyw12gOtZdtoaeIDvL0",
-    authDomain: "ecomm-clone-8906a.firebaseapp.com",
-    projectId: "ecomm-clone-8906a",
-    storageBucket: "ecomm-clone-8906a.appspot.com",
-    messagingSenderId: "920823129111",
-    appId: "1:920823129111:web:45144aab984232e5c16852",
-    measurementId: "G-PE882PM68D"
-  };
+  apiKey: "AIzaSyB9IaAqLHRCwBN8fyw12gOtZdtoaeIDvL0",
+  authDomain: "ecomm-clone-8906a.firebaseapp.com",
+  projectId: "ecomm-clone-8906a",
+  storageBucket: "ecomm-clone-8906a.appspot.com",
+  messagingSenderId: "920823129111",
+  appId: "1:920823129111:web:45144aab984232e5c16852",
+  measurementId: "G-PE882PM68D"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
