f41877ff5acdfc92fec04773c6e440e7d74173db
Ashish <ashishramachandran804@gmail.com>
2020-12-30 16:21:47 -0800

    Added home component with react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      {/* Header */}
-      {/* Home */}
-    </div>
+    <Router>
+      <div className="app">
+
+        <Header />
+        <Switch>
+          
+          <Route path="/home">
+            <Home />
+          </Route>
+
+          <Route path="/checkout">
+            <h1>I AM A CHECKOUT</h1>
+          </Route>
+
+          <Redirect to="/home" />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
